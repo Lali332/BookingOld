68c1293e95d7a6b96b0dd53f1c9fec266941ce32
Lali332 <lalifeldman332@gmail.com>
2023-11-08 18:28:06 +0200

    datacontext

---
+++ Booking/Booking/Entities/DataContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Booking.Entities
+{
+    public class DataContext
+    {
+        public List<Orders> OrdersList { get; set; }
+        public List<Renter> RentersList { get; set; }
+        public List<Zimmer> ZimmersList { get; set; }
+        public int CntOrders { get; set; }
+        public int CntRenters { get; set; }
+        public int CntZimmers { get; set; }
+        public DataContext()
+        {
+            OrdersList = new List<Orders>();
+            RentersList = new List<Renter>();
+            ZimmersList = new List<Zimmer>();
+            CntOrders = 1;
+            CntRenters=1;
+            CntZimmers=1;
+        }
+
+    }
+}
